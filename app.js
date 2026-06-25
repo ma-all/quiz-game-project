541bb4668d7bd19b5e0850c7fa90465985f72467
ma-all <marwaalbaloushi21@gmail.com>
2026-06-26 00:24:58 +0300

    started writing the js code

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 //constants
+const categoryArr = ['Information Systems', 'Software Engineering']
 
 //variables
+let userChoice
+let score = 0
 
 //cache elements
+const question = document.querySelector('#question')
+const questionNum = document.querySelector('#question-number')
+const categoryOne = document.querySelector('#category-One')
+const categoryTwo = document.querySelector('#category-Two')
+const restart = document.querySelector('#restart')
 
 //event listeners
+restart.addEventListener('click', function(){
+
+})
 
 //functions
+function checkScore() {
+
+}
+
+function checkAnswers() {
+
+}
