0c06ea716272194e7aaddac27d474dc30b6f6c5b
ma-all <marwaalbaloushi21@gmail.com>
2026-06-27 19:30:42 +0300

    added new event listsners and functions

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
 //constants
-const categoryArr = ['Information Systems', 'Software Engineering']
+// const categoryArr = ['Information Systems', 'Software Engineering']
+const questionsArr = []
+const winArr = []
 
 //variables
 let userChoice
 let score = 0
+let category 
+let questionShown
 
 //cache elements
 const question = document.querySelector('#question')
 const questionNum = document.querySelector('#question-number')
-const categoryOne = document.querySelector('#category-One')
-const categoryTwo = document.querySelector('#category-Two')
+// const categoryOne = document.querySelector('#category-One')
+// const categoryTwo = document.querySelector('#category-Two')
+const categories = document.querySelectorAll('.categoriesBox')
 const restart = document.querySelector('#restart')
+const answer = document.querySelector('.answersBox')
+const results = document.querySelector('#results')
 
 //event listeners
-restart.addEventListener('click', function(){
+categories.addEventListener('click', function() {
+
+})
+
+question.addEventListener('click', function() {
+    
+})
+
+answer.addEventListener('click', function() {
+
+})
+
+restart.addEventListener('click', function() {
 
 })
 
 //functions
+function showQuestion() {
+
+}
+
+function checkQuestionNum() {
+
+}
+
 function checkScore() {
 
 }
